837b215471a1e8fb94ccb294fca86554e6c07646
pett-pedro <petterson.pedro73@gmail.com>
2022-02-17 19:44:44 -0300

    commit

--- grafo_brasil.py
+++ grafo_brasil.py
@@ -1,4 +1,31 @@
-from pygsp import graphs, filters, plotting
+from pygsp import graphs, plotting
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
+plotting.BACKEND = 'matplotlib'
+
+### Dataframe with only the latitude and longitude informations
+df = pd.read_csv("C:\\Users\\pette\\Desktop\\petinho\\uacsa\\Iniciação Científica\\meus programas\\script_csv_temp\\dados_lat_long_alt_temp ATUALIZADO.csv",
+                 usecols=[0,1])
+
+###Latitude and longitude data columns
+latitude = df.iloc[0:, 0:1]
+longitude = df.iloc[0:, 1:2]
+
+###Dataframe to numpy array
+latitude = latitude.to_numpy()
+longitude = longitude.to_numpy()
+df = df.to_numpy()
+
+###Set the coordinate system as: longitude = x axis and latitude = y axis
+x, y = longitude, latitude
+coords = np.stack([x, y], axis=1)
+
+###Generating a nearest-neighbor graph from point cloud
+G = graphs.NNGraph(df, k= 10, center= True, rescale= True)
+
+###Plot coordinates and plot
+G.set_coordinates(kind= coords)
+G.plot()
+plt.show()
--- grafo_minnesota.py
+++ grafo_minnesota.py
@@ -1,4 +1,29 @@
-from pygsp import graphs, filters, plotting
+from pygsp import graphs, plotting
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+
+plotting.BACKEND = 'matplotlib'
+
+### Generating the built in Minnesota graph and
+### computing its fourier basis 
+g = graphs.Minnesota()
+g.compute_fourier_basis()
+g.plot()
+plt.show()
+
+### Defining the arbitrary signal
+signal = np.cos(g.coords[:, 0]) + np.sin(g.coords[:, 1])
+
+### Plot the signal onto the graph
+g.plot_signal(signal)
+plt.set_cmap('jet')
+plt.show()
+
+### Computing the graph Fourier transform and plotting onto it
+coeff = g.gft(signal)
+g.plot_signal(coeff)
+plt.show()
+
+### Plotting the Fourier transform  
+plt.stem(coeff)
+plt.show()
